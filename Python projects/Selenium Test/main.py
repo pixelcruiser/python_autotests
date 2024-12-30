b4bf6f768852faf41737931ca00d3c4a57944602
pixelcruiser <pixelcruiser@gmail.com>
2024-12-30 03:18:30 +0300

    Add files via upload

--- Python projects/Selenium Test/main.py
+++ Python projects/Selenium Test/main.py
@@ -11,6 +11,27 @@ body_create = {
 
 response_create = requests.post(url = f'{URL}/pokemons', headers = HEADER, json = body_create)
 print(response_create.text)
-
 message = response_create.json()['message']
+print(message)
+
+body_change_name = {
+    "pokemon_id": "151663",
+    "name": "accelgor",
+    "photo_id": -1
+}
+
+response_change_name = requests.put(url=f'{URL}/pokemons', headers = HEADER, json = body_change_name)
+print(response_change_name.text)
+message = response_change_name.json()['message']
+print(message)
+
+
+body_add_pokeball = {
+    "pokemon_id": "175373"
+}
+
+
+response_add_pokeball = requests.post(url=f'{URL}/trainers/add_pokeball', headers = HEADER, json = body_add_pokeball)
+print(response_add_pokeball.text)
+message = response_add_pokeball.json()['message']
 print(message)
